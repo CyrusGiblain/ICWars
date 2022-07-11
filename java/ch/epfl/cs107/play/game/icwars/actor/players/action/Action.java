14936a75c99beb7cc4e4b08564bb6b2444192a96
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-07-11 05:48:47 +0200

    Quelques bugs mais fonctionne jusqu'au joueur IA.

---
+++ java/ch/epfl/cs107/play/game/icwars/actor/players/action/Action.java
@@ -0,0 +1,25 @@
+package ch.epfl.cs107.play.game.icwars.actor.players.action;
+
+import ch.epfl.cs107.play.game.actor.Graphics;
+import ch.epfl.cs107.play.game.icwars.actor.players.ICWarsPlayer;
+import ch.epfl.cs107.play.game.icwars.actor.players.unit.Unit;
+import ch.epfl.cs107.play.game.icwars.area.ICWarsArea;
+import ch.epfl.cs107.play.window.Canvas;
+import ch.epfl.cs107.play.window.Keyboard;
+
+public abstract class Action implements Graphics {
+    Unit unit;
+    ICWarsArea area;
+    String name;
+    int actionKey;
+
+    public Action(Unit unit, ICWarsArea area){
+        this.unit = unit;
+        this.area = area;
+    }
+    public abstract void draw(Canvas canvas);
+    public abstract void doAction(float dt, ICWarsPlayer player, Keyboard keyboard);
+    public abstract String getName();
+    public abstract void doAutoAction(float dt, ICWarsPlayer player, Unit attackedUnit);
+    public int getActionKey(){return actionKey;}
+}
